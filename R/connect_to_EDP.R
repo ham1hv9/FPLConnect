0589315608a507de369ab29e115e6e1730a8d419
ham1hv9 <ham0hv9@fpl.com>
2023-09-13 05:37:50 -0400

    Initial Commit

---
+++ R/connect_to_EDP.R
@@ -0,0 +1,39 @@
+
+#' Connect to EDP on AWS
+#'
+#' @param user: A Character String, The User ID/SLID that needed to connect to EDP.
+#' @param password:  A Character String, User's Password needed to Authenticate and
+#' connect to the EDP database.
+#'
+#' @return Returns A connection string of user ID and password to AWS which allows a user access to the EDP database.
+#'  Must have packages DBI and odbc installed in your Library.
+#' @export
+#'
+#' @examples
+#' con <- connect_to_EDP("ham0hv9", "z95aM72bd2")
+connect_to_EDP <- function(user, password){
+
+  # 1.0 Establish Connection with EDP ----
+
+  print("Connecting to Database!")
+
+  # Connection Script
+  con <-   DBI::dbConnect(odbc::odbc(),
+                          Driver   = "{Amazon Redshift (x64)}",
+                          Server   = "edprsdwpdb1",
+                          Port     = "5610",
+                          Database = "edp",
+                          Uid      = user,
+                          Pwd      = password)
+  # Error Msg:
+  if(!exists("con")){
+    print("Someting is WRONG! Unable to Establish Connection.")
+  }else{
+    print("-- Connection Successfull --")
+  }
+
+
+  return(con)
+
+
+}
